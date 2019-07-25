04500bfa5e1297c6c39f7546c011e914a952e619
hongchaov <2457283655@qq.com>
2019-07-25 20:41:20 +0800

    本次修改内容为蓝色名称

--- jixing_zhongxin.py
+++ jixing_zhongxin.py
@@ -3,9 +3,9 @@ import numpy as np
 #import imutils
 import cv2
 import time
-#设定红色阈值，HSV空间
-redLower = np.array([100, 100, 100])
-redUpper = np.array([120, 255, 255])
+#设定蓝色阈值，HSV空间
+blueLower = np.array([100, 100, 100])
+blueUpper = np.array([120, 255, 255])
 #初始化追踪点的列表
 mybuffer = 64
 pts = deque(maxlen=mybuffer)
@@ -13,7 +13,7 @@ pts = deque(maxlen=mybuffer)
 camera = cv2.VideoCapture(0)
 #等待两秒
 time.sleep(2)
-#遍历每一帧，检测红色瓶盖
+#遍历每一帧，检测蓝色瓶盖
 while True:
     #读取帧
     (ret, frame) = camera.read()
@@ -25,8 +25,9 @@ while True:
     #frame = imutils.resize(frame, width=600)
     #转到HSV空间
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
     #根据阈值构建掩膜
-    mask = cv2.inRange(hsv, redLower, redUpper)
+    mask = cv2.inRange(hsv, blueLower, blueUpper)
     #腐蚀操作
     mask = cv2.erode(mask, None, iterations=2)
     #膨胀操作，其实先腐蚀再膨胀的效果是开运算，去除噪点
@@ -39,27 +40,22 @@ while True:
     if len(cnts) > 0:
         #找到面积最大的轮廓
         c = max(cnts, key = cv2.contourArea)
-        #确定面积最大的轮廓的外接圆
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        #确定面积最大的轮廓的矩形
+        x, y, w, h = cv2.boundingRect(c)
         #计算轮廓的矩
         M = cv2.moments(c)
         #计算质心
         center = (int(M["m10"]/M["m00"]), int(M["m01"]/M["m00"]))
         #只有当半径大于10时，才执行画图
-        if radius > 5:
-            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
-            #把质心添加到pts中，并且是添加到列表左侧
-            pts.appendleft(center)
-    #遍历追踪点，分段画出轨迹
-    for i in range(1, len(pts)):
-        if pts[i - 1] is None or pts[i] is None:
-            continue
-        #计算所画小线段的粗细
-        thickness = int(np.sqrt(mybuffer / float(i + 1)) * 2.5)
-        #画出小线段
-        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-    #res = cv2.bitwise_and(frame, frame, mask=mask)
+        print(center)
+        cv2.rectangle(frame, (x,y),(x+w, y+h), (0, 0, 255), 2)#显示矩形框
+        cv2.circle(frame, center, 1, (0, 0, 255), -1)#显示圆心
+    #转到HSV空间
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    #根据阈值构建掩膜
+    mask = cv2.inRange(hsv, blueLower, blueUpper)
+
     cv2.imshow('Frame', frame)
     #键盘检测，检测到esc键退出
     k = cv2.waitKey(5)&0xFF
